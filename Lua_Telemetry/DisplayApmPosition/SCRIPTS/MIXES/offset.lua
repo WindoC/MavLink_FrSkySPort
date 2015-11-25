9aa59addc928bfb0d7773e995e845fb1a9babf06
Antonio Cheong <windo.ac@gmail.com>
2015-11-25 20:03:15 +0800

    no use

--- Lua_Telemetry/DisplayApmPosition/SCRIPTS/MIXES/offset.lua
+++ Lua_Telemetry/DisplayApmPosition/SCRIPTS/MIXES/offset.lua
@@ -1,3 +1,24 @@
+--
+-- Offset lua
+--
+-- Copyright (C) 2014 Michael Wolkstein
+-- 
+-- https://github.com/Clooney82/MavLink_FrSkySPort
+--
+-- This program is free software; you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation; either version 3 of the License, or
+-- (at your option) any later version.
+--
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY, without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+-- GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License
+-- along with this program; if not, see <http://www.gnu.org/licenses>.
+--
+
 local inputs = { {"O-SET mAh% ", VALUE,-100,100,0}, {"O-SET Wh%", VALUE, -100, 100, 0}, {"BatCap Wh", VALUE, 0, 250, 30} }
 
 local oldoffsetmah=0
